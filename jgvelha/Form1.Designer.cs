67754ae5dc8eab29e936b8f5cacdd2d1937f0592
Home <Home@DESKTOP-HVACMV8>
2025-05-23 17:54:05 -0300

    Adicionar arquivos de projeto.

---
+++ jgvelha/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace jgvelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            b11 = new Button();
+            b12 = new Button();
+            b13 = new Button();
+            b21 = new Button();
+            b22 = new Button();
+            b23 = new Button();
+            b31 = new Button();
+            b32 = new Button();
+            b33 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // b11
+            // 
+            b11.BackColor = Color.Thistle;
+            b11.Location = new Point(23, 23);
+            b11.Name = "b11";
+            b11.Size = new Size(100, 100);
+            b11.TabIndex = 0;
+            b11.Text = " ";
+            b11.UseVisualStyleBackColor = false;
+            // 
+            // b12
+            // 
+            b12.BackColor = Color.Thistle;
+            b12.Location = new Point(129, 23);
+            b12.Name = "b12";
+            b12.Size = new Size(100, 100);
+            b12.TabIndex = 1;
+            b12.UseVisualStyleBackColor = false;
+            // 
+            // b13
+            // 
+            b13.BackColor = Color.Thistle;
+            b13.Location = new Point(235, 23);
+            b13.Name = "b13";
+            b13.Size = new Size(100, 100);
+            b13.TabIndex = 2;
+            b13.UseVisualStyleBackColor = false;
+            // 
+            // b21
+            // 
+            b21.BackColor = Color.Thistle;
+            b21.Location = new Point(23, 129);
+            b21.Name = "b21";
+            b21.Size = new Size(100, 100);
+            b21.TabIndex = 3;
+            b21.UseVisualStyleBackColor = false;
+            // 
+            // b22
+            // 
+            b22.BackColor = Color.Thistle;
+            b22.Location = new Point(129, 129);
+            b22.Name = "b22";
+            b22.Size = new Size(100, 100);
+            b22.TabIndex = 4;
+            b22.UseVisualStyleBackColor = false;
+            // 
+            // b23
+            // 
+            b23.BackColor = Color.Thistle;
+            b23.Location = new Point(235, 129);
+            b23.Name = "b23";
+            b23.Size = new Size(100, 100);
+            b23.TabIndex = 5;
+            b23.UseVisualStyleBackColor = false;
+            // 
+            // b31
+            // 
+            b31.BackColor = Color.Thistle;
+            b31.Location = new Point(23, 235);
+            b31.Name = "b31";
+            b31.Size = new Size(100, 100);
+            b31.TabIndex = 6;
+            b31.UseVisualStyleBackColor = false;
+            // 
+            // b32
+            // 
+            b32.BackColor = Color.Thistle;
+            b32.Location = new Point(129, 235);
+            b32.Name = "b32";
+            b32.Size = new Size(100, 100);
+            b32.TabIndex = 7;
+            b32.UseVisualStyleBackColor = false;
+            // 
+            // b33
+            // 
+            b33.BackColor = Color.Thistle;
+            b33.Location = new Point(235, 235);
+            b33.Name = "b33";
+            b33.Size = new Size(100, 100);
+            b33.TabIndex = 8;
+            b33.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI Black", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(23, 338);
+            label1.Name = "label1";
+            label1.Size = new Size(312, 49);
+            label1.TabIndex = 9;
+            label1.Text = "X, é a sua vez";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LavenderBlush;
+            ClientSize = new Size(358, 396);
+            Controls.Add(label1);
+            Controls.Add(b33);
+            Controls.Add(b32);
+            Controls.Add(b31);
+            Controls.Add(b23);
+            Controls.Add(b22);
+            Controls.Add(b21);
+            Controls.Add(b13);
+            Controls.Add(b12);
+            Controls.Add(b11);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Velha";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button b11;
+        private Button b12;
+        private Button b13;
+        private Button b21;
+        private Button b22;
+        private Button b23;
+        private Button b31;
+        private Button b32;
+        private Button b33;
+        private Label label1;
+    }
+}
